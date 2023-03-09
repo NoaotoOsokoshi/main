bcd73a513aa95205bdedf4f2052e893004908a33
Naoto Osokoshi <osokoshi.kawauso@gmail.com>
2023-03-09 18:13:16 +0900

    sample

---
+++ HelloWorld/src/sample/selfintroduction.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class selfintroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+String name ="獺越　真土";
+int age = 26 ;
+String hobby = "読書";
+
+System.out.println("私の名前は"+ name + "です。");
+System.out.println("年齢は"+ age + "歳です。");
+System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
