d9cac65dfca4edba787b0a77103e0b38d877abef
Naoto Osokoshi <osokoshi.kawauso@gmail.com>
2023-03-13 20:10:22 +0900

    for2

---
+++ for型反復処理/src/for型反復処理/for型反復処理2.java
@@ -0,0 +1,25 @@
+package for型反復処理;
+
+public class for型反復処理2 {
+	/*	
+	問題.for文を使用して九九の計算を出力してください。
+		※出力例 	1×1=1 1×2=2…1×9=9
+					2×1=2 2×2=4…2×9=18
+							・
+							・
+							・
+					9×1=9 9×2=18…9×9=81
+					*/
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 1; j <= 9; j++) {
+				System.out.println(" " + i + "×" + j + "=" + i * j);
+			}
+			System.out.println();
+		}
+	}
+}
