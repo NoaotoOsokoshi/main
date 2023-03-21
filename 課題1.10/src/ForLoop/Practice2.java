25a7a36a68846e167b928cbb8afbd50cfc82bed1
Naoto Osokoshi <osokoshi.kawauso@gmail.com>
2023-03-21 17:46:56 +0900

    practice2

--- 課題1.10/src/ForLoop/Practice2.java
+++ 課題1.10/src/ForLoop/Practice2.java
@@ -17,7 +17,7 @@ public class Practice2 {
 		// TODO 自動生成されたメソッド・スタブ
 		for (int i = 1; i <= 9; i++) {
 			for (int j = 1; j <= 9; j++) {
-				System.out.println(" " + i + "×" + j + "=" + i * j);
+				System.out.print(" " + i + "×" + j + "=" + i * j);
 			}
 			System.out.println();
 		}
