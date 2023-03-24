16147ed4c935bb3f778b4c414edc2c5ac1df01b3
Naoto Osokoshi <osokoshi.kawauso@gmail.com>
2023-03-24 16:44:20 +0900

    Q6　修正

---
+++ 課題1.19/src/basicClass/Animal.java
@@ -0,0 +1,15 @@
+package basicClass;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
+		
+				// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+				
+				// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
+				
+	}
+
+}
