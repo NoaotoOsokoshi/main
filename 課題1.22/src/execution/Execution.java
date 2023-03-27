0916728a0f893bf362a36048d4743afa312ce237
Naoto Osokoshi <osokoshi.kawauso@gmail.com>
2023-03-27 20:02:55 +0900

    パッケージ

--- 課題1.22/src/execution/Execution.java
+++ 課題1.22/src/execution/Execution.java
@@ -1,10 +1,30 @@
 package execution;
-import process.Process;
-public class Execution {
 
+import process.Process;
+
+public class Execution {
+	/*
+	 
+	下記がコンソールに出力されるように作成してください
+	
+		こんにちは！ここは日本です！
+		この寿司はうまい
+		寿司は和食です
+		今の現在日時は2023/03/09 10:23:39です
+	
+	【条件】
+		・JavaProjectを新規作成し、実行用のpackageと処理用のpackageを作成してください。
+		・適切なファイルにフィールドで変数を必要な数作ってください。
+		・thisを使って作成してください。
+		・日時は今日の日付を取得してください。
+		
+	*/
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-Process.hello();
+		String[] str = null;
+
+		Process process = new Process();
+		process.hello(str);
 	}
 
 }
--- 課題1.22/src/process/Process.java
+++ 課題1.22/src/process/Process.java
@@ -4,14 +4,21 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Process {
-	static LocalDateTime ldt = LocalDateTime.now();
-	static DateTimeFormatter dtformat = DateTimeFormatter.ofPattern("yyyy/MM/dd H:mm:ss");
-	static String nowDate = dtformat.format(ldt);
+	static LocalDateTime ldt = LocalDateTime.now();//現在の時間
+	static DateTimeFormatter dtformat = DateTimeFormatter.ofPattern("yyyy/MM/dd H:mm:ss");//出力の仕方
+	static String nowDate = dtformat.format(ldt);//現在時間を出力
 
-	public static void hello() {
-		System.out.println("こんにちは！ここは日本です！");
-		System.out.println("この寿司はうまい");
-		System.out.println("寿司は和食です");
-		System.out.println("今の現在日時は" + nowDate + "です");
+	public String[] str = { "こんにちは！ここは日本です！",
+			"この寿司はうまい",
+			"寿司は和食です",
+			"今の現在日時は" + nowDate + "です"
+	};
+
+	public void hello(String[] str) {
+		for (int i = 0; i < this.str.length; i++) {
+			if (this.str != null) {
+				System.out.println(this.str[i]);
+			}
+		}
 	}
 }
