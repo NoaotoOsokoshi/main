319e78f462e11ae5e3f1dbd2064bcea642cbf3b3
Naoto Osokoshi <osokoshi.kawauso@gmail.com>
2023-04-14 20:50:32 +0900

    途中

---
+++ 課題1.30/src/introduction/Person.java
@@ -0,0 +1,16 @@
+package introduction;
+
+public class Person {
+	// インスタンスフィールドを定義
+String name;
+int age;
+double height;
+	// コンストラクタを定義しインスタンスフィールドに値をセット
+	Person(String name, int age, double height){
+		this.name = name;
+		this.age = age;
+		this.height = height;
+	}
+}
+
+
