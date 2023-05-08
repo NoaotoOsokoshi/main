67d6149ae654e8e15d5a7274c0fcc6858b78edf7
Naoto Osokoshi <osokoshi.kawauso@gmail.com>
2023-05-08 20:07:22 +0900

    kadai

--- kadai/src/lesson33/Person.java
+++ kadai/src/lesson33/Person.java
@@ -1,76 +1,33 @@
 package lesson33;
 
-class Person {
-	public static int count = 0;
-	public String name;
-	
-	public int age;
-	public double height, weight;
+class Person{
+public static int count = 0;
+public String firstName;
+public int age;
+public double height, weight;
 
-	//getter count
-	public static int getCount() {
-		return count;
-	}
-//setter count
-	public static void setCount(int count) {
-		Person.count = count;
-	}
+Person(String firstName, int age, double height, double weight){
+Person.count++;
+this.firstName = firstName;
+this.age = age;
+this.height = height;
+this.weight = weight;
+}
 
-	//getter name
-	public String getName() {
-		return name;
-	}
+public String fullName(){
+return this.firstName + this.lastName;
+}
 
-//setter name
-	public void setName(String lastName) {
-		this.name = lastName;
-	}
+public void print(){
+System.out.println("名前は" + this.fullName() + "です");
+System.out.println("年は" + this.age + "です");
+}
 
-	//getter age
-	public int getAge() {
-		return age;
-	}
+public double bmi(){
+return this.weight / this.height / this.height;
+}
 
-	
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public double getHeight() {
-		return height;
-	}
-
-	public void setHeight(double height) {
-		this.height = height;
-	}
-
-	public double getWeight() {
-		return weight;
-	}
-
-	public void setWeight(double weight) {
-		this.weight = weight;
-	}
-
-	Person(String name, int age, double height, double weight) {
-		Person.count++;
-		this.name = name;
-		this.age = age;
-		this.height = height;
-		this.weight = weight;
-	}
-
-	
-	public void print() {
-		System.out.println("名前は" + this.name + "です");
-		System.out.println("年は" + this.age + "です");
-	}
-
-	public double bmi() {
-		return this.weight / this.height / this.height;
-	}
-
-	public static void printCount() {
-		System.out.println("合計" + Person.count + "人です");
-	}
+public void printCount(){
+System.out.println("合計" + Person.count + "人です");
+}
 }
